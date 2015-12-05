641238ae955a56e84bb5a86f57a080f4031d62e8
vaggelis <vaggelis@Evangeloss-MacBook-Pro.local>
2015-12-05 20:27:37 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var app = express();
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+  console.log('moufa');
+});
+
+
+app.get('/scrape', function (req,res) {
+
+var out='';
+var FeedParser = require('feedparser')
+  , request = require('request');
+
+var req1 = request(req.query.link)
+  , feedparser = new FeedParser();
+req1.on('error', function (error) {
+  // handle any request errors
+});
+req1.on('response', function (res1) {
+  var stream = this;
+
+  if (res1.statusCode != 200) return this.emit('error', new Error('Bad status code'));
+
+  stream.pipe(feedparser);
+});
+
+
+feedparser.on('error', function(error) {
+  // always handle errors
+});
+feedparser.on('readable', function() {
+    var post;
+    
+    while (post = this.read()) {
+      //out=out+(JSON.stringify(post.title+"@"+post.guid+"<br/>", ' ', 4));      
+      out=out+post.description;
+      out=out+"<br/>";
+    }
+});
+
+
+feedparser.on('end', function() {
+    res.send(out);
+});
+
+
+
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
