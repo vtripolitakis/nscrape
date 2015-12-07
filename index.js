8b39e27ef4c04b09d0807739b7aa664c3020a713
Vaggelis <vtripolitakis@gmail.com>
2015-12-07 20:05:53 +0200

    added jade templating

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('views', './views');
+app.set('view engine', 'jade');
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');  
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
 
