8ebf9367f8dabecc1d4842dd2738bbc717567319
Vaggelis <vtripolitakis@gmail.com>
2015-12-07 21:15:51 +0200

    bug fix

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$('.url').each(function () {
     var $this = $(this);
     $this.on("click", function () {
-    	$.get('http://localhost:5000/parse?selector='+$(this).data('selector')+'&link='+$(this).data('url')+'&toRemove='+$(this).data('toremove'),function(data)
+    	$.get('/parse?selector='+$(this).data('selector')+'&link='+$(this).data('url')+'&toRemove='+$(this).data('toremove'),function(data)
 		{
 			$('div.rightColumn').html(data)
 		});        
